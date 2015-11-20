37e96b2ff1c0c524aa48e784a9ffdf5a484b63a3
ekypri08 <ekypri08@cs.ucy.ac.cy>
2015-11-20 21:42:23 +0200

    newtext on string file

--- app/src/main/java/com/example/elinos/epl361winter15team8/MainMenu.java
+++ app/src/main/java/com/example/elinos/epl361winter15team8/MainMenu.java
@@ -2,8 +2,6 @@ package com.example.elinos.epl361winter15team8;
 
 import android.os.Bundle;
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,9 +11,9 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.menu);
+/*        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
