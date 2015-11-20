a798c3906dd8c8502e30fb714101be621c48e46e
yiangos01 <ygeorg01@cs.ucy.ac.cy>
2015-11-20 19:06:31 +0200

    monument.xmi & map.xmi

--- app/src/main/java/com/example/elinos/epl361winter15team8/MyPath.java
+++ app/src/main/java/com/example/elinos/epl361winter15team8/MyPath.java
@@ -10,7 +10,7 @@ public class MyPath extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_path);
+        setContentView(R.layout.monument);
     }
 
     @Override
